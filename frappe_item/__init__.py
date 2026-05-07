0e384a4e7a1e3d59c15ab778bd754db8890a7fb2
Aryan Singh <aquiveal@gmail.com>
2026-05-08 00:04:38 +0530

    refactor(patch): migrate item validation logic to dedicated module
    - Remove local implementation of validate_stock_exists_for_template_item from __init__.py
    - Import and use validate_stock_exists_for_template_item from the project's own doctype module
    - Rename patch_item_variant to patch_item for clarity

--- frappe_item/__init__.py
+++ frappe_item/__init__.py
@@ -1,31 +1,13 @@
 __version__ = "0.0.1"
 
 import frappe
-from frappe.utils import cint
-from frappe import _
 
-def validate_stock_exists_for_template_item_patched(self):
-    from erpnext.stock.doctype.item.item import StockExistsForTemplate
-    
-    if self.stock_ledger_created() and self._doc_before_save:
-        if (
-            cint(self._doc_before_save.has_variants) != cint(self.has_variants)
-            or self._doc_before_save.variant_of != self.variant_of
-        ):
-            frappe.throw(
-                _(
-                    "Cannot change Variant properties after stock transaction. You will have to make a new Item to do this."
-                ).format(self.name),
-                StockExistsForTemplate,
-            )
-
-        # Removed the attributes check to allow changes even after stock transactions.
-
-def patch_item_variant():
+def patch_item():
     try:
         import erpnext.controllers.item_variant
         import erpnext.stock.doctype.item.item
         from frappe_item.overrides.controllers.item_variant import make_variant_item_code, copy_attributes_to_variant
+        from frappe_item.frappe_item.doctype.item.item import validate_stock_exists_for_template_item
 
         erpnext.controllers.item_variant.make_variant_item_code = make_variant_item_code
         erpnext.stock.doctype.item.item.make_variant_item_code = make_variant_item_code
@@ -33,8 +15,8 @@ def patch_item_variant():
         erpnext.stock.doctype.item.item.copy_attributes_to_variant = copy_attributes_to_variant
         
         # Monkey patch validate_stock_exists_for_template_item
-        erpnext.stock.doctype.item.item.Item.validate_stock_exists_for_template_item = validate_stock_exists_for_template_item_patched
+        erpnext.stock.doctype.item.item.Item.validate_stock_exists_for_template_item = validate_stock_exists_for_template_item
     except ImportError:
         pass
 
-patch_item_variant()
+patch_item()
