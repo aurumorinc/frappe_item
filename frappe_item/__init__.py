57c65c85e5005ddf8dad0baa9cfc58013bdfb861
Aryan Singh <aquiveal@gmail.com>
2026-05-07 23:53:23 +0530

    fix(item): allow item variant attribute changes
    Monkey patch validate_stock_exists_for_template_item to remove strict attribute validation, permitting changes to item variants even after stock transactions have been created.

--- frappe_item/__init__.py
+++ frappe_item/__init__.py
@@ -1,6 +1,25 @@
 __version__ = "0.0.1"
 
 import frappe
+from frappe.utils import cint
+from frappe import _
+
+def validate_stock_exists_for_template_item_patched(self):
+    from erpnext.stock.doctype.item.item import StockExistsForTemplate
+    
+    if self.stock_ledger_created() and self._doc_before_save:
+        if (
+            cint(self._doc_before_save.has_variants) != cint(self.has_variants)
+            or self._doc_before_save.variant_of != self.variant_of
+        ):
+            frappe.throw(
+                _(
+                    "Cannot change Variant properties after stock transaction. You will have to make a new Item to do this."
+                ).format(self.name),
+                StockExistsForTemplate,
+            )
+
+        # Removed the attributes check to allow changes even after stock transactions.
 
 def patch_item_variant():
     try:
@@ -12,6 +31,9 @@ def patch_item_variant():
         erpnext.stock.doctype.item.item.make_variant_item_code = make_variant_item_code
         erpnext.controllers.item_variant.copy_attributes_to_variant = copy_attributes_to_variant
         erpnext.stock.doctype.item.item.copy_attributes_to_variant = copy_attributes_to_variant
+        
+        # Monkey patch validate_stock_exists_for_template_item
+        erpnext.stock.doctype.item.item.Item.validate_stock_exists_for_template_item = validate_stock_exists_for_template_item_patched
     except ImportError:
         pass
 
